02bb87f386e4d0804cfb9d8736594342322de6ca
Matt Ao <mattyayoh@gmail.com>
2014-01-18 13:52:42 -0500

    Initial Commit

---
+++ CollegeLivin/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CollegeLivin
+//
+//  Created by Matthew Ao on 1/18/14.
+//  Copyright (c) 2014 Matthew Ao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
