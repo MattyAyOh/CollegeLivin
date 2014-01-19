8b1a34f164af4b14bd60c75fe43d6a458259a0dd
Matt Ao <mattyayoh@gmail.com>
2014-01-19 15:38:46 -0500

    Misc files and pictures

---
+++ CollegeLivin/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  CollegeLivin
+//
+//  Created by Matthew Ao on 1/18/14.
+//  Copyright (c) 2014 Matthew Ao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Test : UICollectionViewController
+
+@end
