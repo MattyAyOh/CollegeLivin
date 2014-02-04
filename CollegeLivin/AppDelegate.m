c27ab5ca141c976074fc209349d9dc10701a0951
latne1ec <latne1ec@cmich.edu>
2014-02-04 10:21:37 -0500

    1. changed navigation bar color from light grey to black shade. 2. changed background color from black to light grey. 3. changed color of status bar characters to white.

--- CollegeLivin/AppDelegate.m
+++ CollegeLivin/AppDelegate.m
@@ -7,11 +7,18 @@
 //  -evan 1st test for github-
 
 #import "AppDelegate.h"
+#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]];
+
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    
+    [[UINavigationBar appearance] setBarTintColor:UIColorFromRGB(0x1F1F21);
+     
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];     
+    
     return YES;
 }
 
